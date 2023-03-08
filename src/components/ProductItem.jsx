ea3052c456ca4e962a8b3c5aa8e12a002c9c5f46
Damian Contreras orellana <damian.contreras036@gmail.com>
2023-03-07 21:30:17 -0300

    Functional add to cart to order in views

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,31 +1,29 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '../styles/ProductItem.scss'
+import addToCartImage from 'icons/bt_add_to_cart.svg';
 
-import addToCart from 'icons/bt_add_to_cart.svg';
+const ProductItem = ({ product }) => {
+	const { addToCart } = useContext(AppContext);
 
-const ProductItem = ({product}) => {
-
-	const [cart, setCart] = useState([]);
-
-	const handleClick = () => {
-		setCart([]);
+	const handleClick = item => {
+		addToCart(item);
 	}
 
-    return (
-        <div className="ProductItem">
+	return (
+		<div className="ProductItem">
 			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={handleClick}>
-					<img src={addToCart} alt="" />
+				<figure onClick={() => handleClick(product)} >
+					<img src={addToCartImage} alt="" />
 				</figure>
-				{cart}
 			</div>
 		</div>
-    );
+	);
 }
 
 export default ProductItem;
